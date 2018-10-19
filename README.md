4b0e0c9dfacd88e9b15ee425bfedf65d542f3c62
Sam Peters <samp805@gmail.com>
2018-10-19 13:23:53 -0400

    example json

---
+++ README.md
@@ -0,0 +1,6 @@
+To run, make sure your Bluetooth is on.
+
+Buttons:
+8 = A
+4 = B
+12 = A + B
--- init_wii.py
+++ init_wii.py
@@ -1,5 +1,6 @@
 import cwiid
 import time
+import json
 
 def main():
     _ = raw_input('Press enter after pressing 1 + 2 on Wiimote\n')
@@ -9,11 +10,14 @@ def main():
         print 'Failed to connect ... is Bluetooth on?'
     
     print 'Connection successful'
-    time.sleep(0.5) # sleep for a half second
 
     # print initial state
     wii.rpt_mode = cwiid.RPT_ACC | cwiid.RPT_IR | cwiid.RPT_BTN
+    time.sleep(1) # let the sensors wake up
     print 'Initial state:  ' + str(wii.state)
+
+    with open('./data/example.json', 'w+') as f:
+        f.write(json.dumps(wii.state, sort_keys=True, indent=4))
 
     return wii
 
