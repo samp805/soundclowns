4684dd34b56c28a9228ced1be2e15b1ada2f45d0
Sam Peters <samp805@gmail.com>
2018-11-08 18:16:35 -0500

    data & rpt wii added

---
+++ rpt_wii.py
@@ -0,0 +1,50 @@
+import cwiid
+import time
+import json
+
+
+def connect():
+    _ = raw_input('Press enter after pressing 1 + 2 on Wiimote\n')
+    try:
+        wii = cwiid.Wiimote()
+    except(RuntimeError):
+        print 'Failed to connect ... is Bluetooth on?'
+        raise
+    
+    print 'Connection successful'
+
+    # print initial state
+    wii.rpt_mode = cwiid.RPT_ACC | cwiid.RPT_IR | cwiid.RPT_BTN
+    time.sleep(1) # let the sensors wake up
+    print 'Initial state:  ' + str(wii.state)
+
+    with open('./data/example.json', 'w+') as f:
+        f.write(json.dumps(wii.state, sort_keys=True, indent=4))
+
+    return wii
+
+def report(state):
+    with open('data.json', 'a+') as f:
+        f.write(json.dumps(state.get('ir_src')) + '\n')
+
+def poll(wii):
+    try:
+        old_state = wii.state
+        while(True):
+            b_pressed = (wii.state.get('buttons') == 4)
+            current_state = wii.state
+            if b_pressed and (old_state != current_state):
+                report(wii.state)
+                # print(wii.state)
+                old_state = current_state
+    except(KeyboardInterrupt):
+        print('Quitting...')
+
+def cleanup(wii):
+    del wii
+    print 'Wii disconnected'
+
+if __name__ == "__main__":
+    wii = connect()
+    poll(wii)
+    cleanup(wii)
